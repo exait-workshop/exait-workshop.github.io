2fef9b607b70a871f7495b322989a5307d08d2af
lenarttreven <lenart.treven44@gmail.com>
2025-04-01 14:14:25 +0000

    Deploying to gh-pages from @ exait-workshop/exait-workshop.github.io@0407b5633afb7799f5324ba2c241a7a25454adb9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/accepted_papers/";
           },
-        },{id: "nav-speakers",
-          title: "Speakers",
-          description: "Our speakers.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/speakers/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
@@ -416,7 +409,57 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "organizers-amy-x-lu",
+          title: 'Amy_x_lu',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/amy_x_lu/";
+            },},{id: "organizers-andreas-krause",
+          title: 'Andreas_krause',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/andreas_krause/";
+            },},{id: "organizers-andrew-wagenmaker",
+          title: 'Andrew_wagenmaker',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/andrew_wagenmaker/";
+            },},{id: "organizers-bhavya-sukhija",
+          title: 'Bhavya_sukhija',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/bhavya_sukhija/";
+            },},{id: "organizers-carmelo-sferrazza",
+          title: 'Carmelo_sferrazza',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/carmelo_sferrazza/";
+            },},{id: "organizers-kevin-jamieson",
+          title: 'Kevin_jamieson',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/kevin_jamieson/";
+            },},{id: "organizers-lenart-treven",
+          title: 'Lenart_treven',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/lenart_treven/";
+            },},{id: "organizers-olivier-bachem",
+          title: 'Olivier_bachem',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/olivier_bachem/";
+            },},{id: "organizers-parnian-kassraie",
+          title: 'Parnian_kassraie',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/parnian_kassraie/";
+            },},{id: "organizers-pieter-abbeel",
+          title: 'Pieter_abbeel',
+          description: "",
+          section: "Organizers",handler: () => {
+              window.location.href = "/organizers/pieter_abbeel%20/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
@@ -461,6 +504,46 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "speakers-alison-gopnik",
+          title: 'Alison_gopnik',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/alison_gopnik/";
+            },},{id: "speakers-benjamin-van-roy",
+          title: 'Benjamin_van_roy',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/benjamin_van_roy/";
+            },},{id: "speakers-dorsa-sadigh",
+          title: 'Dorsa_sadigh',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/dorsa_sadigh/";
+            },},{id: "speakers-dylan-foster",
+          title: 'Dylan_foster',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/dylan_foster/";
+            },},{id: "speakers-ji-won-park",
+          title: 'Ji_won_park',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/ji_won_park/";
+            },},{id: "speakers-masatoshi-uehara",
+          title: 'Masatoshi_uehara',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/masatoshi_uehara/";
+            },},{id: "speakers-rishabh-agarwal",
+          title: 'Rishabh_agarwal',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/rishabh_agarwal/";
+            },},{id: "speakers-sergey-levine",
+          title: 'Sergey_levine',
+          description: "",
+          section: "Speakers",handler: () => {
+              window.location.href = "/speakers/sergey_levine/";
             },},{
         id: 'social-email',
         title: 'email',
